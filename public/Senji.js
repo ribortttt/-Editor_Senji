823a986350c0030ded3f879cfb6a7432b9f29bf1
Reybort Cuaton <163703861+ribortttt@users.noreply.github.com>
2026-05-20 02:14:48 +0800

    Add files via upload

---
+++ public/Senji.js
@@ -0,0 +1,73 @@
+document.querySelector(".Reels-vid").classList.add("loaded");{
+
+    const video = document.getElementById("my-video");
+    const playBtn = document.getElementById("play-btn");
+    const frames = document.querySelectorAll("iframe");
+    const cards = document.querySelectorAll(".card");
+
+    video.addEventListener("loadeddata", () => {
+        document
+        .querySelector(".Reels-vid")
+        .classList.add("loaded");
+    });
+    playBtn.addEventListener("click", () => {
+        if (video.paused) {
+
+            video.play();
+            playBtn.textContent = "❚❚";
+        } else {
+
+            video.pause();
+            playBtn.textContent = "▶";
+        }
+    });
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }});}, {
+    threshold: 0.2
+});
+frames.forEach(frame => {
+    observer.observe(frame);
+});
+frames.forEach((frame, index) => {
+    frame.style.transitionDelay = `${index * 0.1}s`;
+});
+
+cards.forEach(card => {
+
+    card.addEventListener("mousemove", (e) => {
+
+        const rect = card.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        const rotateX = ((y - centerY) / centerY) * -12;
+        const rotateY = ((x - centerX) / centerX) * 12;
+
+        card.style.transform = `
+            perspective(1200px)
+            rotateX(${rotateX}deg)
+            rotateY(${rotateY}deg)
+            scale(1.05)
+        `;
+    });
+    card.addEventListener("mouseleave", () => {
+        card.style.transform = `
+            perspective(1200px)
+            rotateX(0deg)
+            rotateY(0deg)
+            scale(1)
+        `;
+    });
+
+});
+
+};
